43f4f522dfefbbd0e598212dcbeb50cfc2331eaa
surya-git <suryansha34@gmail.com>
2025-03-02 11:46:32 +0530

    Streamlit app dependencies

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import streamlit as st
-import numpy as np
 import tensorflow as tf
-from sklearn.preprocessing import StandardScaler,LabelEncoder,OneHotEncoder
 
 # Load the trained model
 model = tf.keras.models.load_model('Sentiment_analyse_RFC_model.pkl')
